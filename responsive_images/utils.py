c8c4800fcbf1f33efda8594962eb5f8678fa52b9
Bhaskar <bhaskar@agiliq.com>
2013-07-08 19:51:10 +0530

    Refactor code

--- responsive_images/utils.py
+++ responsive_images/utils.py
@@ -1,12 +1,21 @@
 from django.conf import settings
 from django.core.servers.basehttp import FileWrapper
+from django.contrib.staticfiles import finders
 from django.http import HttpResponse
 
+
+from PIL import Image
+
 import os
+from subprocess import call
 
 
-def get_final_resolution(resolution):
-    resolution = int(resolution)
+def get_final_resolution(request):
+    resolution = None
+    try:
+        resolution = int(request.COOKIES['resolution'])
+    except KeyError:
+        return 100
     try:
         resolutions = settings.RESPONSIVE_IMAGE_RESOLUTIONS
     except AttributeError:
@@ -21,19 +30,47 @@ def get_final_resolution(resolution):
                 break
     if resolution < resolutions[-1]:
         final_resolution = resolutions[-1]
-    final_resolution = final_resolution - 50
     return final_resolution
 
 
-def get_file(filename, extension):
-    f = file(filename, "rb")
+def get_file(filename_with_path, extension):
+    f = file(filename_with_path, "rb")
     wrapper = FileWrapper(f)
     if extension not in ['png', 'jpg', 'jpeg', 'gif']:
         mimetype = 'image/jpeg'
     else:
         mimetype = 'image/%s' % extension
     response = HttpResponse(wrapper, mimetype=mimetype)
-    response['Content-Length'] = os.path.getsize(filename)
+    response['Content-Length'] = os.path.getsize(filename_with_path)
     response['Content-Disposition'] = \
-        'attachment; filename={0}'.format(filename)
+        'attachment; filename={0}'.format(filename_with_path.split("/").pop())
     return response
+
+def get_resized_image(filename, final_resolution):
+    fullname = finders.find(filename)
+    filename = filename.split("/").pop()
+    filename, extension = filename.split(".")
+    i = Image.open(fullname)
+    aspect_ratio = i.size[1]/float(i.size[0])
+    final_width = final_resolution
+    final_height = int(final_resolution*aspect_ratio)
+
+    if i.size[0] < final_resolution:
+        return fullname, extension
+    filename = "%s_%s_%s.%s" % (filename, final_width, final_height,
+                             extension)
+
+    cache_dir_name = getattr('settings', 'RESPONSIVE_IMAGES_CACHE_DIR',
+                             'responsive_images_cache')
+    cache_path = os.path.join(settings.MEDIA_ROOT, cache_dir_name)
+    if not finders.find(cache_dir_name):
+        os.mkdir(cache_path)
+
+    resized_image_full_path = os.path.join(cache_path, filename)
+
+    if not finders.find(cache_dir_name + "/" + filename):
+        size = (final_width, final_height)
+        resized_image = i.resize(size, Image.ANTIALIAS)
+        resized_image.save(resized_image_full_path, extension,
+            quality=getattr('settings', cache_dir_name, 75)) 
+    return resized_image_full_path, extension
--- responsive_images/views.py
+++ responsive_images/views.py
@@ -1,20 +1,12 @@
 from django.conf import settings
-from django.contrib.staticfiles import finders
 
-from responsive_images.utils  import get_final_resolution, get_file
-
-from PIL import Image
-
-import os
-from subprocess import call
+from responsive_images.utils import (
+    get_final_resolution, get_file, get_resized_image
+)
 
 
 def adapt_image(request):
-    try:
-        resolution = int(request.COOKIES['resolution'])
-        final_resolution = get_final_resolution(resolution)
-    except KeyError:
-        final_resolution = 100
+    final_resolution = get_final_resolution(request)
 
     filename = request.META['QUERY_STRING'].replace(settings.IMAGE_ENDS_WITH, "")
 
@@ -24,34 +16,8 @@ def adapt_image(request):
         filename = filename[len(settings.MEDIA_URL):]
     else:
         return None
-    
-    fullname = finders.find(filename)
-    filename = filename.split("/").pop()
-    filename, extension = filename.split(".")
-    i = Image.open(fullname)
-    aspect_ratio = i.size[1]/float(i.size[0])
-    final_width = final_resolution
-    final_height = int(final_resolution*aspect_ratio)
 
-    if max(i.size[0], i.size[1]) < final_resolution:
-        return get_file(fullname, extension)
-    filename = "%s_%s_%s.%s" % (filename, final_width, final_height,
-                             extension)
-
-    cache_dir_name = getattr('settings', 'RESPONSIVE_IMAGES_CACHE_DIR',
-                             'responsive_images_cache')
-    cache_path = os.path.join(settings.MEDIA_ROOT, cache_dir_name)
-    if not finders.find(cache_dir_name):
-        os.chdir(settings.MEDIA_ROOT)
-        call(['mkdir', cache_dir_name])
-    os.chdir(cache_path)
-
-    if not finders.find(cache_dir_name + "/" + filename):
-        size = (final_width, final_height)
-        resized_image = i.resize(size, Image.ANTIALIAS)
-        resized_image.save(filename, extension,
-            quality=getattr('settings', cache_dir_name, 75)) 
+    resized_filename, extension = get_resized_image(filename, final_resolution)
 
     # Return resized image
-    return get_file(filename, extension)
-
+    return get_file(resized_filename, extension)
