df748e3b90bf846b3b6933c34269ee778a088663
Bhaskar <bhaskar@agiliq.com>
2013-08-14 16:35:35 +0530

    Added EXTRA_STATIC_DIRECTORY to settings

--- example/settings.py
+++ example/settings.py
@@ -64,7 +64,9 @@ STATIC_ROOT = '/home/agiliq/build/agiliq/agiliqcom/static/responsive'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/responsive/'
+EXTRA_STATIC_DIRECTORY = 'responsive'
+STATIC_URL = '/static/' + EXTRA_STATIC_DIRECTORY + '/'
+
 
 # Additional locations of static files
 STATICFILES_DIRS = (
