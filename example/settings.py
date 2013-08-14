ded8e5bf0c9addd7fe6b8c97acf31439737de818
Bhaskar <bhaskar@agiliq.com>
2013-08-14 15:45:18 +0530

    Modified static root

--- example/settings.py
+++ example/settings.py
@@ -60,7 +60,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/agiliq/build/agiliq/agiliqcom/static/responsive'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
